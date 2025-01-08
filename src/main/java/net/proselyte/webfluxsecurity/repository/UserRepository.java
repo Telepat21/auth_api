2ec98404659c724bed48f05f936f041fd95babf5
Roman Maksymovych <roma.maks777.rm@gmail.com>
2025-01-08 17:34:27 +0200

    initial commit

---
+++ src/main/java/net/proselyte/webfluxsecurity/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package net.proselyte.webfluxsecurity.repository;
+
+import net.proselyte.webfluxsecurity.entity.UserEntity;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+public interface UserRepository extends ReactiveCrudRepository<UserEntity, Long> {
+
+    Mono<UserEntity> findByUsername(String username);
+}
