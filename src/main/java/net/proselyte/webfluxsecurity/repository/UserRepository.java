8e02bd37d578d3e5050c25c6ab471faebf52f59f
Roman Maksymovych <roma.maks777.rm@gmail.com>
2025-01-16 08:32:06 +0200

    Add PATCH method

--- src/main/java/net/proselyte/webfluxsecurity/repository/UserRepository.java
+++ src/main/java/net/proselyte/webfluxsecurity/repository/UserRepository.java
@@ -7,4 +7,5 @@ import reactor.core.publisher.Mono;
 public interface UserRepository extends ReactiveCrudRepository<UserEntity, Long> {
 
     Mono<UserEntity> findByUsername(String username);
+    Mono<UserEntity> updateUser(String email);
 }
